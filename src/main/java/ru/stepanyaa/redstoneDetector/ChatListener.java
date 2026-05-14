8897aae6a0b700e93c9ce28fb9da8f31ad81608c
Stepanyaa <stipakarpov76@gmail.com>
2026-05-14 20:27:42 +0300

    Fixed the year in the license.

--- src/main/java/ru/stepanyaa/redstoneDetector/ChatListener.java
+++ src/main/java/ru/stepanyaa/redstoneDetector/ChatListener.java
@@ -2,7 +2,7 @@
  * MIT License
  *
  * RedstoneDetector
- * Copyright (c) 2026 Stepanyaa
+ * Copyright (c) 2025 Stepanyaa
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
